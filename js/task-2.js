9f0358b9c7324eb0f36d73d7c47557f0fb00bd79
ElenaYa <elena.yatsynyuk@gmail.com>
2023-10-10 09:45:36 +0300

    adding use strict

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,4 @@
+'use sctrict';
 function getShippingMessage(country, price, deliveryFee) {
     const totalPrice = price + deliveryFee;
     return `Shipping to ${country} will cost ${totalPrice} credits`;
--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,4 @@
+'use sctrict';
 function getElementWidth(content, padding, border) {
     return parseFloat(content) + parseFloat(padding)*2 + parseFloat(border)*2;
 }
